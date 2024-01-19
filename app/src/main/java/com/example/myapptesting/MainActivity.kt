c91cece8676a292cd221766e1bcc1f6a2344d6ba
AwaisJans <awais.muhammad.jans@gmail.com>
2024-01-19 19:58:57 +0500

    Added an AAR in App Testing

---
+++ app/src/main/java/com/example/myapptesting/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myapptesting
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.window.SplashScreen
+import com.example.splashscrrenrequestexample.activities.SplashLibScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, SplashLibScreen::class.java))
+
+        finish()
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/myapptesting/MyConfig.kt
@@ -0,0 +1,37 @@
+package com.example.splashscrrenrequestexample
+
+class MyConfig {
+    // String
+    var internalVariable = "App is Controlling the Lib2"
+    var name = "MasterMind-App"
+
+    // Int
+    var age = 30
+
+    // Boolean
+    var isOld = false
+
+    // List
+    var list = listOf("Mango",
+        "Apple",
+        "Chocolate",
+        "Banana",
+        "Carrot",
+        "Grapes",
+        "Strawberry")
+
+    // Tint Color
+    var tintColor = "#fab1ed"
+
+    // Font Size
+    var fontSize = 40
+
+    // Image Height
+    var imageHeight = 200
+
+    // Image Width
+    var imageWidth = 200
+
+
+
+}
