2622cea9f534fc4e61c5b708e57ec05d6697a0a1
AwaisJans <awais.muhammad.jans@gmail.com>
2024-02-25 17:12:19 +0500

    added a new aab file

--- app/src/main/java/com/example/myapptesting/MainActivity.kt
+++ app/src/main/java/com/example/myapptesting/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.example.myapptesting
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import com.example.splashscrrenrequestexample.MyConfig
+import androidx.appcompat.app.AppCompatActivity
 import com.example.splashscrrenrequestexample.activities.SplashLibScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+
 
         startActivity(Intent(this, SplashLibScreen::class.java))
         finish()
